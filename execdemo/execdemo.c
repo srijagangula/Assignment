5061e0eb36c235ba5f533ee6e611c8cb82758772
srijagangula <srija.gangula08@gmail.com>
2018-09-07 11:11:54 +0530

    Race Condition  Added

---
+++ execdemo/execdemo.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <unistd.h>
+int main()
+{
+pid_t id;
+printf("before fork %d\n",getpid());
+id=fork();
+if(id==0)
+{
+printf("child proces is %d\n",getpid());
+execl("/bin/ls","/bin/ls","-a","-l",NULL);
+printf("after exec");
+}
+else
+{
+printf("parent\n");
+
+}
+}
+
